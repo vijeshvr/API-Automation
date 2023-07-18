7b3799ff22c4296336dcafd86f957f95dceb69b5
Vijesh V R <vijeshvr182@gmail.com>
2023-07-18 10:33:06 +0530

    First Commit api

---
+++ src/main/java/testRunner/MyTestRunner.java
@@ -0,0 +1,20 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    features = "C:\\Users\\Vijesh V R\\eclipse-workspace\\BDD\\src\\main\\resources\\features\\apitest.feature",
+    glue = {"stepDefinitions"},
+//    tags = "@tag",
+    dryRun = true,
+    plugin = {"pretty", "html:target/cucumber-reports"}
+)
+
+
+public class MyTestRunner {
+
+}
